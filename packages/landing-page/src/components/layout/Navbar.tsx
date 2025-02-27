6d638725388eb7e92b9b9c3ad793ad669a564f85
B0bbyBrown <6obbybrown@gmail.com>
2025-02-27 13:44:31 +0200

    ok

--- packages/landing-page/src/components/layout/Navbar.tsx
+++ packages/landing-page/src/components/layout/Navbar.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import styles from "../styles/Navbar.module.css";
+import styles from "../Styles/Navbar.module.css";
 import Link from "next/link";
 import { useAuth } from "../../contexts/AuthContext";
 
@@ -7,29 +6,31 @@ export default function Navbar() {
   const { user, logout } = useAuth();
 
   return (
-    <nav className={styles.navbar}>
-      <div className="logo">ESL Explorers</div>
-      <ul className="nav-links">
-        {user ? (
-          <>
-            <li>
-              <button onClick={logout}>Logout</button>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <Link href="/login/student/">Student Login</Link>
-            </li>
-            <li>
-              <Link href="/login/teacher/">Teacher Login</Link>
-            </li>
-            <li>
-              <Link href="/register/">Register</Link>
-            </li>
-          </>
-        )}
-      </ul>
-    </nav>
+    <>
+      <nav className={styles.navbar}>
+        <div className="logo">ESL Explorers</div>
+        <ul className="nav-links">
+          {user ? (
+            <button>
+              <li>
+                <button onClick={logout}>Logout</button>
+              </li>
+            </button>
+          ) : (
+            <>
+              <li>
+                <Link href="/login/student/">Student Login</Link>
+              </li>
+              <li>
+                <Link href="/login/teacher/">Teacher Login</Link>
+              </li>
+              <li>
+                <Link href="/register/">Register</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </>
   );
 }
